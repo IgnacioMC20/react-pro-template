ab3062a395ebc2be3a8bf7170a2a69fb4cac7c06
ignaciocuyun <ignacio.cuyun@telus.com>
2022-12-21 12:54:06 -0600

    template finished

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, NavLink, Navigate, Route, Routes } from "react-router-dom"
+
+import logo from '../logo.svg'
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React Logo" />
+
+          {/* Routes */}
+          <ul>
+            <li>
+              <NavLink to='/' className={({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to='/about' className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to='/users' className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          {/* <Route path="about" element={<h1>About Page</h1>}></Route> */}
+          {/* <Route path="users" element={<h1>Users Page</h1>}></Route> */}
+          {/* <Route path="/" element={<h1>Home Page</h1>}></Route> */}
+
+          {/* todo: 404 page */}
+          {/* <Route path="/*" element={<Navigate to={'/home'} replace />}></Route> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
