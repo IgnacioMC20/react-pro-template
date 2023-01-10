e12d7511492ad80784a57214861499dcdf452f12
ignaciocuyun <ignacio.cuyun@telus.com>
2023-01-09 23:35:51 -0600

    branch initialized

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,61 +1,19 @@
 import { ProductButtons, ProductCart, ProductImage, ProductTitle } from "../components"
 import { products } from "../data/products"
-import { useShoppingCart } from "../hooks/useShoppingCart"
-
+const product = products[0];
 export const ShoppingPage = () => {
-
-  const { shoppingCart, onProductCartChange} = useShoppingCart();
 
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
-      <div style={{
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap'
-      }}>
-        {/* <ProductCart product={product} >
-          <ProductCart.Image className="custom-image" />
-          <ProductCart.Title />
-          <ProductCart.Buttons />
-        </ProductCart> */
-        /* <ProductCart
-          product={product}
-          style={{
-            backgroundColor: '#70D1F8'
-          }}>
-          <ProductImage />
+
+        <ProductCart product={product} className="bg-dark text-white">
+          <ProductImage className="custom-image" />
           <ProductTitle />
-          <ProductButtons />
-        </ProductCart> */}
+          <ProductButtons className="custom-buttons" />
+        </ProductCart>
 
-        {
-          products.map(p => (
-            <ProductCart key={p.id} product={p} className="bg-dark text-white" value={shoppingCart[p.id]?.count || 0} onChange={(e) => onProductCartChange(e)}>
-              <ProductImage className="custom-image" />
-              <ProductTitle />
-              <ProductButtons className="custom-buttons" />
-            </ProductCart>
-          ))
-        }
-
-        <div className="shopping-cart">
-          {
-            Object.entries(shoppingCart).map(([key, product]) => (
-              <ProductCart key={key} product={product} value={product.count} className="bg-dark text-white" style={{ width: '100px' }} onChange={(e) => onProductCartChange(e)}>
-                <ProductImage className="custom-image" />
-                <ProductButtons className="custom-buttons" />
-              </ProductCart>
-            ))
-          }
-        </div>
-      </div>
-      <div>
-        <code>
-          {JSON.stringify(shoppingCart, null, 5)}
-        </code>
-      </div>
     </div>
   )
 }
